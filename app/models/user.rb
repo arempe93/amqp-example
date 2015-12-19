77ffea664622b638cfb5caf4419a3f14927ceab6
Andrew Rempe <andrewrempe@gmail.com>
2015-12-19 17:25:16 -0500

    Adds User methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,4 +27,41 @@ class User < ActiveRecord::Base
     ## Devise
     devise :database_authenticatable, :registerable,
         :recoverable, :trackable, :validatable
+
+    ## Callbacks
+    before_create :create_xhcg
+
+    ## Validations
+    validates :email, uniqueness: { case_sensitive: false }
+
+    ## Relationships
+    has_many :devices
+
+    ## Private Methods
+    private
+    def create_xhcg
+
+        # generate exchange name
+        self.amqp_xchg = "xchg.user.#{self.id}"
+
+        begin
+
+            # create in rmq
+            AMQP::Factory.create_exchange self.amqp_xchg, durable: true
+
+        rescue => e
+
+            # log error
+            Rails.logger.error "#<User id:#{self.id}>.create_xchg raised => '#{e.message}'"
+            Rails.logger.error "#{e.backtrace}"
+
+            # bubble up call stack
+            raise
+
+        else
+
+            # continue creation
+            true
+        end
+    end
 end
