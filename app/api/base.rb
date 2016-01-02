7346959ea093def74aa79ded2db1b62cfd56e2c0
Andrew Rempe <andrewrempe@gmail.com>
2016-01-02 18:00:20 -0500

    Adds users api

--- app/api/base.rb
+++ app/api/base.rb
@@ -11,11 +11,14 @@ module API
 			header['Access-Control-Allow-Origin'] = '*'
 			header['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
 			header['Access-Control-Allow-Headers'] = 'accept, content-type'
+        end
 
+        after_validation do
             @user, @device = authenticate! if declared(params).key?(:auth_token)
         end
 
         mount Auth
+        mount Users
 
         add_swagger_documentation hide_format: true
     end
---
+++ app/api/users.rb
@@ -0,0 +1,78 @@
+module API
+    class Users < Grape::API
+
+		represent User, with: API::Entities::User
+        represent Device, with: API::Entities::Device
+
+		resource :users do
+
+			desc 'Sign up a new user'
+			params do
+				requires :email, type: String
+				requires :password, type: String
+				requires :username, type: String
+				optional :name, type: String
+
+				group :device, type: Hash do
+                    requires :uuid, type: String
+                    requires :user_agent, type: String
+                    optional :mobile, type: Boolean, default: false
+                end
+			end
+			post do
+
+				# create user
+				user = User.new email: params[:email], password: params[:password], username: params[:username], name: params[:name]
+
+				# create device
+                device = Device.generate params[:device].to_hash.merge({ user: user })
+
+                # get un-hashed auth token
+                auth_token = device.token_hash
+
+                begin
+
+					# save user
+					user.save!
+
+                    # save new device
+                    device.save!
+
+				rescue ActiveRecord::ActiveRecordError => e
+
+					unprocessable '422.1', e.message
+
+                rescue => e
+
+                    server_error! '500.1', e.message
+
+                else
+
+                    # present token and user
+                    present :auth_token, auth_token
+                    present :device, device
+                end
+			end
+
+			desc 'Search users to chat with'
+			params do
+				optional :query, type: String, default: ''
+				optional :auth_token, type: String
+			end
+			get do
+
+				# normalize query
+				query = params[:query].downcase
+
+				# find users
+				users = User.where 'lower(username) like :query or lower(name) like :query', query: "%#{query}%"
+
+				# limit to 20 results
+				users = users.limit 20
+
+				# present results
+				present :users, users
+			end
+		end
+	end
+end
