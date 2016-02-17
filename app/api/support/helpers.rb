9fed85c44d4062cfc9dd1a14b73039d1d04296c8
Andrew Rempe <andrewrempe@gmail.com>
2016-02-17 10:25:34 -0500

    Change to 2 spaces

--- app/api/support/helpers.rb
+++ app/api/support/helpers.rb
@@ -1,16 +1,16 @@
 module API
-	module Support
-		module Helpers
-		
-			def set(params)
-				params.delete :auth_token
-				declared(params).to_hash.compact.deep_symbolize_keys
-			end
+  module Support
+    module Helpers
 
-			def validate!(model, code = '422')
-				unprocessable! code, "#{model.class.name}: #{model.errors.full_messages.join(', ')}" unless model.valid?
-			end
+      def set(params)
+        params.delete :auth_token
+        declared(params).to_hash.compact.deep_symbolize_keys
+      end
 
-		end
-	end
+      def validate!(model, code = '422')
+        unprocessable! code, "#{model.class.name}: #{model.errors.full_messages.join(', ')}" unless model.valid?
+      end
+
+    end
+  end
 end
